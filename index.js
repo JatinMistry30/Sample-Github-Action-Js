63276c2facade0873f1e5c33409fd07cbc7f81d9
jatin9975 <jatinmistry9975@gmail.com>
2024-12-07 20:22:15 +0530

    Add workflow and index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("Hello world!");  // This is a simple JavaScript code that outputs "Hello world!"
+console.log("Hello world!"); 
+// This is a simple JavaScript code that outputs "Hello world!"
+
+console.log("Changes done by test1 branch")
